35b386875bf9cee11ea1ee5ff207376d79969ea9
chavezbryanr <chavezbryanr@gmail.com>
2020-01-16 21:38:39 -0600

    Starting over
    Starting over and starting simple. First I want to process data (by csv) then keep working from there.

--- src/reader/DataReader.java
+++ src/reader/DataReader.java
@@ -11,17 +11,17 @@ public class DataReader {
 	public static List<Product> getProductList() {
 		List<Product> productList = new ArrayList<Product>();
 		//Read products from somewhere and store it into productList
-		
-		
+
+
 		return productList;
 	}
-	
-	
-	
+
+
+
 	public static List<Invoice> createInvoiceList() {
 		List<Invoice> invoiceList = new ArrayList<Invoice>();
-		
-		
+
+
 		return invoiceList;
 	}
 
